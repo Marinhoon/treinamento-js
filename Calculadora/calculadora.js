fcb81e64f8e16ef3ed243eba68adbaf3ae3a0ac0
Vitor Marinho <vitormarinhoh@gmail.com>
2024-01-24 16:03:37 -0300

    Treinamento JS

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,15 @@
+const entrada1 = prompt("Informe o primeiro número:")
+const entrada2 = prompt("Informe o segundo número:")
+
+const soma = x + y
+const subtracao = x - y
+const multiplicacao = x * y
+const divisao = x / y
+
+alert (
+    "Resultados \n" +
+    "\nSome: " + soma +
+    "\nSubtração: " + subtracao +
+    "\nMultiplicação: " + multiplicacao +
+    "\nDivisão: " + divisao 
+)
---
+++ Recrutas/recrutas.js
@@ -0,0 +1,18 @@
+const primeiroNome = prompt("Informe o primeiro nome")
+const sobrenome = prompt ("Informe o sobrenome do Recruta")
+const campoDeEstudo = prompt ("Qual é o campo de estudo do recruta?")
+const anoDeNascimento = prompt ("Qual é o ano de nascimento do recruta?")
+
+
+alert(
+    "Recruta Cadastrado com Sucesso! \n" +
+    "\n Nome Completo: " + primeiroNome + " " + sobrenome +
+    "\n Campo de estudo: " + campoDeEstudo +
+    "\n Idade: " + (2024 - anoDeNascimento)
+)
+
+console.log (primeiroNome)
+console.log (sobrenome)
+console.log (campoDeEstudo)
+console.log (anoDeNascimento)
+
